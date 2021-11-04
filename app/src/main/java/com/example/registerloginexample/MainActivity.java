887580efe1d5e7ba3e07fe3823201ddbf31bff84
skfl0282 <90668082+skfl0282@users.noreply.github.com>
2021-11-04 13:56:25 +0900

    Initial commit

--- app/src/main/java/com/example/registerloginexample/MainActivity.java
+++ app/src/main/java/com/example/registerloginexample/MainActivity.java
@@ -23,9 +23,6 @@ public class MainActivity extends AppCompatActivity {
         String userPass = intent.getStringExtra("userPass");
 
         tv_id.setText(userID);
-        tv_pass.setText(userPass);
 
-        tv_id.setText(userID);
-        tv_pass.setText(userPass);
     }
 }
